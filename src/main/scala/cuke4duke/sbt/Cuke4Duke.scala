df3d318f47e277eaa75fc909f465af75f2aea242
Luke Amdor <luke@absoluterubbish.net>
2009-11-04 12:27:37 -0600

    Organizing into overridable and not meant to be overridable

--- src/main/scala/cuke4duke/sbt/Cuke4Duke.scala
+++ src/main/scala/cuke4duke/sbt/Cuke4Duke.scala
@@ -5,19 +5,18 @@ import java.io.File
 
 trait Cuke4Duke extends BasicManagedProject with ScalaPaths {
   def cuke4DukeVersion = "0.1.8"
-  def cucumberGem = "cucumber --version 0.4.3 --source http://gems.rubyforge.org/"
+  def cuke4DukeGems = List("cucumber --version 0.4.3 --source http://gems.rubyforge.org/")
+  def cuke4DukeJvmArgs = List("-Dcuke4duke.objectFactory=cuke4duke.internal.jvmclass.PicoFactory")
+  def jRubyHome = info.projectPath / "lib_managed" / "cuke4duke_gems"
+  def featuresDirectory = info.projectPath / "features"
+  def extraCucumberOptions: List[String] = Nil
 
   val cuke4DukeRepo = "Cuke4Duke Maven Repository" at "http://cukes.info/maven"
   val cuke4DukeDependency = "cuke4duke" % "cuke4duke" % cuke4DukeVersion % "test"
   val picoContainerDependency = "org.picocontainer" % "picocontainer" % "2.8.3" % "test"
 
-  val cuke4DukeJvmArgs = List("-Dcuke4duke.objectFactory=cuke4duke.internal.jvmclass.PicoFactory")
-
-  def jRubyHome = info.projectPath / "lib_managed" / "cuke4duke_gems"
   val gemPath = jRubyHome / "gems"
   val cucumberBin = gemPath / "bin" / "cucumber"
-
-  def featuresDirectory = info.projectPath / "features"
 
   private def jruby(args: List[String]): Int = {
     if (!jRubyHome.exists) {
@@ -28,29 +27,37 @@ trait Cuke4Duke extends BasicManagedProject with ScalaPaths {
     val classpathArg = (jrubyClasspath.getFiles ++ FileUtilities.scalaJars).map(_.getAbsolutePath).mkString(File.pathSeparator)
     val javaArgs = cuke4DukeJvmArgs ++ ("-classpath" :: classpathArg :: "org.jruby.Main" :: args.toList)
     val env = Map("GEM_PATH" -> gemPath.absolutePath,
-                  "HOME"     -> jRubyHome.absolutePath,
-                  "JRUBY_PARENT_CLASSPATH" -> classpathArg)
+                  "HOME"     -> jRubyHome.absolutePath)
 
     log.debug("Cuke4Duke jvmArgs:\n\t" + javaArgs.mkString("\n\t"))
 
     Fork.java(None, javaArgs, None, env, LoggedOutput(log))
   }
 
-  def installCucumberGem() {
-    log.info("Installing cucumber gem...")
-    jruby(List("-S", "gem", "install", "--no-ri", "--no-rdoc", "--install-dir", gemPath.absolutePath) ++ cucumberGem.split("\\s+"))
+  private def installGem(gem: String) = {
+    jruby(List("-S", "gem", "install", "--no-ri", "--no-rdoc", "--install-dir", gemPath.absolutePath) ++ gem.split("\\s+"))
+  }
+
+  def installCuke4DukeGems() = {
+    log.info("Installing Cuke4Duke gems...")
+    cuke4DukeGems.map(installGem(_)).reduceLeft(_ + _)
   }
 
   lazy val updateNoInstallCucumberGem = super.updateAction
-  lazy val updateGems = task { installCucumberGem; None }
-  override def updateAction = updateGems dependsOn(updateNoInstallCucumberGem)
+  lazy val updateCuke4DukeGems = task {
+    installCuke4DukeGems match {
+      case 0 => None
+      case _ => Some("Installing Cuke4Duke gems failed!")
+    }
+  }
+  override def updateAction = updateCuke4DukeGems dependsOn(updateNoInstallCucumberGem)
 
   def runCucumberFeatures() = {
      jruby(List("-r", "cuke4duke",
                 cucumberBin.absolutePath,
                 featuresDirectory.absolutePath,
                 "--require", testCompilePath.absolutePath,
-                "--color"))
+                "--color") ++ extraCucumberOptions)
   }
 
   lazy val features = task {
