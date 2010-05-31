64c1031f8d3e3396e41d81248beb35d01c519cda
Luke Amdor <luke.amdor@gmail.com>
2010-05-30 21:26:46 -0500

    Updating to sbt 0.7.4

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -1,6 +1,6 @@
 import sbt._
 
 class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
-  val scriptedDep = "org.scala-tools.sbt" % "scripted" % "0.7.0"
-  val databinder_repo = "Databinder Repository" at "http://databinder.net/repo"
+  val scriptedDep = "org.scala-tools.sbt" % "scripted" % "0.7.4"
+  val databinder_repo = Resolver.url("Databinder Repository", new java.net.URL("http://databinder.net/repo"))(Resolver.ivyStylePatterns)
 }
